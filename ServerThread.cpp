576f8ad189ae43c61d34120c56539a21b1237abb
Rafał Frączek <rafalfr@protonmail.ch>
2015-08-25 20:50:15 +0200

    update

---
+++ ServerThread.cpp
@@ -0,0 +1,49 @@
+#include "ServerThread.h"
+#include <map>
+#include <string>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+#include "InterfaceStats.h"
+#include "InterfaceSpeedMeter.h"
+
+using namespace std;
+
+extern map<string, map<string, map<string, InterfaceStats> > > all_stats;
+extern map<string, InterfaceSpeedMeter> speed_stats;
+
+//https://www.abc.se/~m6695/udp.html
+//http://en.cppreference.com/w/cpp/links/libs
+
+void* ServerThread::Thread ( void* arg )
+{
+	int sockfd, n;
+	struct sockaddr_in servaddr, cliaddr;
+	socklen_t len;
+	char mesg[256];
+
+	sockfd = socket ( AF_INET, SOCK_DGRAM, 0 );
+
+	bzero ( &servaddr, sizeof ( servaddr ) );
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl ( INADDR_ANY );
+	servaddr.sin_port = htons ( 32000 );
+	bind ( sockfd, ( struct sockaddr * ) &servaddr, sizeof ( servaddr ) );
+
+	while ( true )
+	{
+		len = sizeof ( cliaddr );
+		memset ( mesg, 0, 256 );
+		n = recvfrom ( sockfd, mesg, 256, 0, ( struct sockaddr * ) &cliaddr, &len );
+		//sendto ( sockfd, mesg, n, 0, ( struct sockaddr * ) &cliaddr, sizeof ( cliaddr ) );
+		mesg[n] = 0;
+		string command(mesg);
+		if (command.compare("get speed")==0)
+        {
+
+        }
+		printf ( "%s\n", mesg );
+	}
+}
---
+++ ServerThread.h
@@ -0,0 +1,11 @@
+#ifndef SERVERTHREAD_H
+#define SERVERTHREAD_H
+
+
+class ServerThread
+{
+    public:
+        static void * Thread ( void *arg );
+};
+
+#endif // SERVERTHREAD_H
