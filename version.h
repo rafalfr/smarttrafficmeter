8e715502d8ee2b64f1cd351b4519004b593aed0c
Rafał Frączek <rafalfr@protonmail.ch>
2017-12-29 23:48:50 +0100

    update

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 1;
-	static const long BUILD  = 1236;
-	static const long REVISION  = 6924;
+	static const long BUILD  = 1238;
+	static const long REVISION  = 6937;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 1254;
-	#define RC_FILEVERSION 2,1,1236,6924
-	#define RC_FILEVERSION_STRING "2, 1, 1236, 6924\0"
-	static const char FULLVERSION_STRING [] = "2.1.1236.6924";
+	static const long BUILDS_COUNT  = 1255;
+	#define RC_FILEVERSION 2,1,1238,6937
+	#define RC_FILEVERSION_STRING "2, 1, 1238, 6937\0"
+	static const char FULLVERSION_STRING [] = "2.1.1238.6937";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 36;
+	static const long BUILD_HISTORY  = 38;
 	
 
 }
